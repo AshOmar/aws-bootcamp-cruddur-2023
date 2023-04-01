d594ef0b3d0b5aff132dfffc242ddfe4d5fd32cc
Ashraf Omar <ashraf.aomar@gmail.com>
2023-04-01 00:30:37 +0000

    db.py and home activities

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,5 +1,8 @@
 from datetime import datetime, timedelta, timezone
 
+#Postgres
+from lib.db import pool, query_wrap_array
+
 #HoneyComb ----------------------
 from opentelemetry import trace
 # -------------------------------
@@ -75,6 +78,32 @@ class HomeActivities:
           'replies': []
         }
         results.insert(0,extra_crud)
+
+      sql = query_wrap_array("""
+            SELECT
+              activities.uuid,
+              users.display_name,
+              users.handle,
+              activities.message,
+              activities.replies_count,
+              activities.reposts_count,
+              activities.likes_count,
+              activities.reply_to_activity_uuid,
+              activities.expires_at,
+              activities.created_at
+            FROM public.activities
+            LEFT JOIN public.users ON users.uuid = activities.user_uuid
+            ORDER BY activities.created_at DESC
+            """)
+      print(sql)
+      with pool.connection() as conn:
+        with conn.cursor() as cur:
+          cur.execute(sql)
+          # this will return a tuple
+          # the first field being the data
+          json = cur.fetchone()
+      return json[0]
+
       #HoneyComb ------------------------------------------
       span.set_attribute("app.result.length",len(results))
       #----------------------------------------------------
