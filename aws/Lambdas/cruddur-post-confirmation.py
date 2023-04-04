7fd1069c8f8a655e11f89145e2fd0a319e9da7c4
Ashraf Omar <ashraf.aomar@gmail.com>
2023-04-04 00:12:00 +0000

    Create Lambda file

---
+++ aws/Lambdas/cruddur-post-confirmation.py
@@ -0,0 +1,34 @@
+import json
+import psycopg2
+
+def lambda_handler(event, context):
+    user = event['request']['userAttributes']
+    try:
+        print("User:============")
+        print(user)
+
+        print("event:=============")
+        print(event)
+
+        print("context:===========")
+        print(context)
+
+        conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
+        sql = f"""
+        INSERT INTO users (display_name, handle,email, cognito_user_id)
+        VALUES(%s, %s, %s)"
+        """
+        cur = conn.cursor()
+        cur.execute(sql, (user['name'], user['email'], user['preferred_name'], user['sub']))
+        conn.commit() 
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        
+    finally:
+        if conn is not None:
+            cur.close()
+            conn.close()
+            print('Database connection closed.')
+
+    return event
