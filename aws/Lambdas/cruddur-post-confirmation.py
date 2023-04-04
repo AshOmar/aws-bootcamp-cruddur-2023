1cfbd2cc98cc3adc9b7b92e1e085669c7d7274e8
Ashraf Omar <ashraf.aomar@gmail.com>
2023-04-04 22:50:31 +0000

    Cognito Lambda to RDS

--- aws/Lambdas/cruddur-post-confirmation.py
+++ aws/Lambdas/cruddur-post-confirmation.py
@@ -14,13 +14,22 @@ def lambda_handler(event, context):
         print("context:===========")
         print(context)
 
-        conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
         sql = f"""
-        INSERT INTO users (display_name, handle,email, cognito_user_id)
-        VALUES(%s, %s, %s)"
+        INSERT INTO public.users (display_name, handle,email, cognito_user_id)
+        VALUES(%(name)s,%(handle)s, %(email)s, %(userID)s)
         """
+        print ("SQL ============")
+        print(sql)
+
+        print("Details =====================================")
+        print('name: ', user['name']) 
+        print('handle: ', user['preferred_username']) 
+        print('email: ', user['email'])
+        print('userID: ', user['sub'])
+
+        conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
         cur = conn.cursor()
-        cur.execute(sql, (user['name'], user['email'], user['preferred_name'], user['sub']))
+        cur.execute(sql, {'name':user['name'], 'handle':user['preferred_username'], 'email':user['email'], 'userID':user['sub']})
         conn.commit() 
 
     except (Exception, psycopg2.DatabaseError) as error:
